ded4988a68e20e7f599ab029314ef6e3d21e36a5
luongvu15595 <luongvu15595@gmail.com>
2017-02-10 07:47:30 +0700

    login

--- book_review/config/routes.rb
+++ book_review/config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get "/static_pages/*page", to: "static_pages#show"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   resources :users , only: [:show]
 end
